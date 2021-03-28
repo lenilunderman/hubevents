ed2f9335bb28eb74840e570959214e1a470f2e84
Leni Lunderman <lenilunderman@Lenis-Mac-mini.local>
2021-03-28 15:15:44 -0400

    Finished the event item component, fully styled.

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -4,6 +4,12 @@ import Link from "next/link";
 // import the css module to this especif component, the only required is to have .module.css at the end of the css, then you can access the classes on css by accessing the name of the class.
 import classes from "./event-item.module.css";
 
+// imports UI components
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import Arrowright from "../icons/arrow-right-icon";
+
 function EventItem(props) {
   // object destructuring to pull out the data from props
   const { title, image, date, location, id } = props;
@@ -24,14 +30,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address> {formattedAddress} </address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}> Explore Event </Link>
+          <Button link={exploreLink}>
+            <span>Explore Events</span>
+            <span className={classes.icon}>
+              <Arrowright />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
