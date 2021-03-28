59507ea743a9877a3ba1dfd12bcfaf30341dae24
Leni Lunderman <lenilunderman@Lenis-Mac-mini.local>
2021-03-28 15:59:33 -0400

    Wrapping the application with navbar, children props and more.

---
+++ components/layout/main-header.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+// import the css
+import classes from "./main-header.module.css";
+
+function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/"> Next Events </Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href="/events"> Check all events </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainHeader;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      {/* The reason you can wrap the layout in here, is because inside layout.js we pass {props.children} , 
+      allowing to use components wraped up*/}
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
